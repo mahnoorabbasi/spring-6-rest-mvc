57c41708b5ea2679419dc0f34fced3c5fcce3935
mahnoor <mabbasi@xavor.com>
2024-02-23 16:08:34 +0500

    mockito test for post of beer and customer

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -1,9 +1,12 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
 import guru.springframework.spring6restmvc.services.BeerService;
 import guru.springframework.spring6restmvc.services.BeerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -21,6 +24,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.hamcrest.core.Is.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@SpringBootTest
@@ -30,11 +34,39 @@ class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper; //spring provided jackson object mapper with sensible defautls
+
     @MockBean
     BeerService beerService;
 
-    BeerServiceImpl beerServiceImp=new BeerServiceImpl();
+    BeerServiceImpl beerServiceImp;
 
+    @BeforeEach
+    public void setBeerServiceImp()
+    {
+        beerServiceImp=new BeerServiceImpl();//setting here so that for each test, its a new object
+    }
+
+    @Test
+    public void testSaveNewBeer() throws Exception {
+        Beer beer=beerServiceImp.listBeers().get(0);
+        beer.setVersion(null);
+        beer.setId(null);
+
+        given(beerService.saveNewBeer(any(Beer.class))).willReturn(beerServiceImp.listBeers().get(1));
+
+        mockMvc.perform(post("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(beer)))
+//                .andExpect(jsonPath("$.id", is(beerServiceImp.listBeers().get(1).getId()))) //in post we are not returing any object
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+
+
+    }
     @Test
     void getBeerList() throws Exception {
         given(beerService.listBeers()).willReturn(beerServiceImp.listBeers());
--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,8 +1,12 @@
 package guru.springframework.spring6restmvc.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.spring6restmvc.model.Beer;
 import guru.springframework.spring6restmvc.model.Customer;
+import guru.springframework.spring6restmvc.services.BeerServiceImpl;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,18 +20,44 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.hamcrest.core.Is.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @WebMvcTest(CustomerController.class)
 public class CustomerControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper; //spring provided jackson object mapper with sensible defautls
 
     @MockBean
     CustomerService customerService;
 
-    CustomerServiceImpl customerServiceImp=new CustomerServiceImpl();
+    CustomerServiceImpl customerServiceImp;
+    @BeforeEach
+    public void setBeerServiceImp()
+    {
+        customerServiceImp=new CustomerServiceImpl();//setting here so that for each test, its a new object
+    }
+    @Test
+    public void testSaveNewBeer() throws Exception {
+        Customer customer=customerServiceImp.getAllCustomers().get(0);
+        customer.setVersion(null);
+        customer.setId(null);
 
+        given(customerService.saveNewCustomer(any(Customer.class))).willReturn(customerServiceImp.getAllCustomers().get(1));
+
+        mockMvc.perform(post("/api/v1/customer")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(customer)))
+//                .andExpect(jsonPath("$.id", is(beerServiceImp.listBeers().get(1).getId()))) //in post we are not returing any object
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"));
+
+
+
+    }
     @Test
     public void getCustomerList() throws Exception {
 
