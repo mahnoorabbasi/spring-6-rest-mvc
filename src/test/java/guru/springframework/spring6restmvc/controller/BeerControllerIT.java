38e4ef76a5e740d562d8a9ee5c44bf9539d760a2
mahnoor <mabbasi@xavor.com>
2024-02-29 22:29:28 +0500

    update by id test cases

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.mappers.BeerMapper;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repository.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -23,7 +24,29 @@ class BeerControllerIT {
     BeerRepository beerRepository;
     @Autowired
     BeerController beerController;
+    @Autowired
+    BeerMapper beerMapper;
 
+
+
+
+    @Rollback
+    @Transactional
+    @Test
+    void updateExistingBeer() {
+        Beer beer = beerRepository.findAll().get(0);
+        BeerDTO beerDTO = beerMapper.beerToBeerDto(beer);
+        beerDTO.setId(null);
+        beerDTO.setVersion(null);
+        final String beerName = "UPDATED";
+        beerDTO.setBeerName(beerName);
+
+        ResponseEntity responseEntity = beerController.updateById(beer.getId(), beerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Beer updatedBeer = beerRepository.findById(beer.getId()).get();
+        assertThat(updatedBeer.getBeerName()).isEqualTo(beerName);
+    }
     @Test
     void testNotFoundUpdateBeer() {
         assertThrows(NotFoundException.class,()->
--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6restmvc.controller;
 
 import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -24,6 +26,9 @@ class CustomerControllerIT {
     @Autowired
     CustomerController customerController;
 
+    @Autowired
+    CustomerMapper customerMapper;
+
     @Test
     void testNotFoundUpdateCustomer() {
         assertThrows(NotFoundException.class, ()->{
@@ -31,6 +36,24 @@ class CustomerControllerIT {
                     .name("bruf")
                     .build());
         });
+    }
+    @Rollback
+    @Transactional
+    @Test
+    void testUpdateCustomer() {
+        Customer customer=customerRepository.findAll().get(0);
+        CustomerDTO customerDTO=customerMapper.customerToCustomerDto(customer);
+        customerDTO.setId(null);
+        customerDTO.setVersion(null);
+
+        String updatedName="Updated";
+        customerDTO.setName(updatedName);
+
+        ResponseEntity responseEntity=customerController.updateCustomerByID(customer.getId(), customerDTO);
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(204));
+
+        Customer updatedCustomer=customerRepository.findById(customer.getId()).get();
+        assertThat(updatedCustomer.getName()).isEqualTo(updatedName);
     }
 
     @Rollback
